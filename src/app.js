b583576619b52763f8dcc490925e8e269137f3ca
zippy <tzipporamiller@gmail.com>
2025-06-04 19:42:24 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return <div>Hello from App.js in builder-project-2!</div>;
+  return <div>Hello from Builder-ready React App!</div>;
 }
 
 export default App;
